95193a5649a1705353d5790367710efce731b285
moritzbuck <mortz.buck@nbis.com>
2018-11-28 10:41:21 +0100

    finished mag processing with stat

--- snakefiles/mags.py
+++ snakefiles/mags.py
@@ -27,51 +27,75 @@ rule phylophlan :
     IFS=$"\n"; for r in `cat data/ppafull.tax.txt`; do id=`echo ${{r}} | cut -f1`; tax=`echo ${{r}} | cut -f2`; sed -i "s/${{id}}/${{id}}_${{tax}}/g" output/{wildcards.type}{wildcards.name}/{wildcards.type}{wildcards.name}.tree.int.nwk; done; unset IFS
     cp output/{wildcards.type}{wildcards.name}/{wildcards.type}{wildcards.name}.tree.int.nwk $DD/{output}
     """
-prokka --outdir /scratch/soda-23-Unterer-Stinkersee_megahit_metabat_bin-2127/soda-23-Unterer-Stinkersee_megahit_metabat_bin-2127   --force --prefix soda-23-Unterer-Stinkersee_megahit_metabat_bin-2127 --locustag soda-23-Unterer-Stinkersee_megahit_metabat_bin-2127 --cpus 20 1000_processed_reads/soda-23-Unterer-Stinkersee/assemblies/megahit/binning/metabat/bins/bin_2127.fasta
+
+
+def mag_stat(folder, bin_head) :
+    bin_checkm = pjoin(folder , "checkm.txt")
+    bin_genome= pjoin(folder ,bin_head + ".fna")
+    bin_proteom = pjoin(folder ,bin_head + ".faa")
+    out_dict = {}
+
+    if "unbinned" not in bin_head:
+        checkm_fields = ['Bin Id', 'Marker lineage', 'UID', '# genomes', '# markers', '# marker sets', '0', '1', '2', '3', '4', '5+', 'Completeness', 'Contamination', 'Strain heterogeneity']
+        with open(bin_checkm) as handle:
+            dat = handle.readlines()
+        dat = {l.split()[0] : {k : v for k,v in zip(checkm_fields, l[:-1].split() ) } for l in dat[3:-1]}
+        dat = dat[bin_head]
+
+        out_dict['completeness'] = float(dat['Completeness'])
+        out_dict['contamination'] = float(dat['Contamination'])
+        out_dict['taxo:checkm'] = dat['Marker lineage']
+        out_dict['strain_heterogeneity'] = float(dat['Strain heterogeneity'])
+    else :
+        out_dict['completeness'] = None
+        out_dict['contamination'] = None
+        out_dict['taxo:checkm'] = 'Unbinned'
+        out_dict['strain_heterogeneity'] = None
+
+
+    with open( bin_genome) as handle:
+        fna = [s for s in SeqIO.parse(handle, "fasta")]
+    with open( bin_proteom) as handle:
+        faa = [s for s in SeqIO.parse(handle, "fasta")]
+
+    out_dict['length'] = sum([len(s.seq) for s in fna])
+    out_dict['nb_contigs'] = len(fna)
+    out_dict['nb_proteins'] = len(faa)
+    out_dict['coding_density'] = (3.0*sum([len(s.seq) for s in faa]))/sum([len(s.seq) for s in fna])
+    out_dict['GC'] = float(sum([str(s.seq).count("G")+str(s.seq).count("C") for s in fna]))/out_dict['length']
+    #out_dict['taxo:phylophlan'] = tax[bin_id]
+    return out_dict
+
 
 rule annotate_all_mags :
     input : folder = "{path}/{set}/assemblies/{assembler}/binning/{binner}/bins"
-    output : folder = "{path}/{set}/assemblies/{assembler}/binning/{binner}/clean_bins"
+    output : folder = "{path}/{set}/assemblies/{assembler}/binning/{binner}/clean_bins",
+             stats = "{path}/{name}/{assembler}/MAGs/{name}.magstats"
     threads = 20
     run :
+        from Bio import SeqIO
+        from tqdm import tqdm
+        from pandas import DataFrame
+
         bins = [f for f in os.listdir(input.folder) if f.endswith(".fasta")]
         os.makedirs(output.folder)
 
         prokka_line = "prokka --outdir {temp_out}/{prefix}  {meta} --force --prefix {prefix} --locustag {prefix} --cpus {threads} {bins}"
+        checkm_line = "checkm lineage_wf -t {threads} -x fna {temp_out}/{prefix} {temp_out}/{prefix}/data > {temp_out}/{prefix}/checkm.txt"
+        out_dict = {}
 
         for b in bins:
             meta = "--metagenome" if b == "bin-unbinned.fasta" else ""
             b_name = b[:-6].replace("_", "-" )
             prefix = "{set}_{assembler}_{binner}_{bin}".format(**wildcards, bin = b_name)
-            prok = prokka_line.format(temp_out = config['general']['temp_dir'], meta = "", prefix = prefix, threads = threads, bins = pjoin(input.folder, b) )
-
-
-
-    shell : """
-
-    input_dir=`dirname {input}`
-
-    for b in `ls $input_dir/*.fa`
-    do
-        bin_id=`basename $b | sed "s#metabat_{wildcards.name}-\\(.*\\).fa#\\1#"`
-        out_dir=$input_dir/metabat_{wildcards.name}_$bin_id
-
-        mkdir -p $out_dir
-
-        if [ $bin_id = 'unbinned' ]
-        then
-            prokka --outdir /scratch/{wildcards.name}_$bin_id/  --metagenome --force --prefix {wildcards.name}_$bin_id --locustag {wildcards.name}_$bin_id --cpus {threads} $b
-            mv /scratch/{wildcards.name}_$bin_id/* $out_dir/
-            touch $out_dir/{wildcards.name}_$bin_id.checkm
-        else
-            prokka --outdir /scratch/{wildcards.name}_$bin_id/  --force --prefix {wildcards.name}_$bin_id --locustag {wildcards.name}_$bin_id --cpus {threads} $b
-            mv /scratch/{wildcards.name}_$bin_id/* $out_dir/
-
-            checkm lineage_wf -t {threads} -x fna $out_dir $out_dir/data > $out_dir/{wildcards.name}_$bin_id.checkm
-            rm -r $out_dir/data
-        fi
-    done
-    """
+            prok = prokka_line.format(temp_out = config['general']['temp_dir'], meta = meta, prefix = prefix, threads = threads, bins = pjoin(input.folder, b) )
+            call(prok, shell = True)
+            if meta == "":
+                call(checkm_line.format(threads= threads, temp_out = config['general']['temp_dir'], prefix = prefix), shell = True)
+            shutil.move("{temp_out}/{prefix}".format(temp_out = config['general']['temp_dir'], prefix = prefix), ouput.folder)
+            shutil.rmtree(pjoin(output.folder, prefix, "data"))
+            out_dict[prefix] = mag_stat(pjoin(output.folder, prefix), prefix)
+        DataFrame.from_dict(out_dict, orient = 'index').to_csv(output.stats)
 
 
 
@@ -101,62 +125,13 @@ rule filter_good_MAGs :
 
 rule MAG_stats:
     input : bin_file = "{path}/{name}/{assembler}/MAGs/metabat_{name}_unbinned/{name}_unbinned.checkm",
-    output : "{path}/{name}/{assembler}/MAGs/{name}.magstats"
     threads : 1
     run :
-        from Bio import SeqIO
-        from tqdm import tqdm
-        from pandas import DataFrame
 
-        out_dict = {}
-        mag_folder = "/".join(input.bin_file.split("/")[:-2])
-        mag_folder = "{path}/{name}/{assembler}/MAGs/".format(path = wildcards.path, name=wildcards.name, assembler = wildcards.assembler)
-        bins = [f.split("-")[-1][:-3] for f in os.listdir(mag_folder) if f.endswith(".fa")]
-        bin_files = [mag_folder + "metabat_{name}_{bin}/{name}_{bin}.checkm".format(name=wildcards.name, bin = f)  for f in bins]
-
-        out_file = output[0]
 
 #        with open("phylophlan_tax.txt") as handle:
 #            tax = [l.split()[:2] for l in handle.readlines()]
 #            tax = {t[0] : ":".join([tt for tt in t[1].split(".") if "?" not in tt ]) for t in tax if "all_samples-" in t[0]}
-        def process_bin(binl) :
-            bin_head = binl[:-7]
-            bin_checkm = bin_head + ".checkm"
-            bin_genome= bin_head + ".fna"
-            bin_proteom = bin_head + ".faa"
-            bin_id = bin_head.split("/")[-1]
-            out_dict = {}
-
-            if "unbinned" not in bin_id:
-                checkm_fields = ['Bin Id', 'Marker lineage', 'UID', '# genomes', '# markers', '# marker sets', '0', '1', '2', '3', '4', '5+', 'Completeness', 'Contamination', 'Strain heterogeneity']
-                with open(bin_checkm) as handle:
-                    dat = handle.readlines()
-                dat = {l.split()[0] : {k : v for k,v in zip(checkm_fields, l[:-1].split() ) } for l in dat[3:-1]}
-                dat = dat[bin_id]
-
-                out_dict['completeness'] = float(dat['Completeness'])
-                out_dict['contamination'] = float(dat['Contamination'])
-                out_dict['taxo:checkm'] = dat['Marker lineage']
-                out_dict['strain_heterogeneity'] = float(dat['Strain heterogeneity'])
-            else :
-                out_dict['completeness'] = None
-                out_dict['contamination'] = None
-                out_dict['taxo:checkm'] = 'Unbinned'
-                out_dict['strain_heterogeneity'] = None
-
-
-            with open( bin_genome) as handle:
-                fna = [s for s in SeqIO.parse(handle, "fasta")]
-            with open( bin_proteom) as handle:
-                faa = [s for s in SeqIO.parse(handle, "fasta")]
-
-            out_dict['length'] = sum([len(s.seq) for s in fna])
-            out_dict['nb_contigs'] = len(fna)
-            out_dict['nb_proteins'] = len(faa)
-            out_dict['coding_density'] = (3.0*sum([len(s.seq) for s in faa]))/sum([len(s.seq) for s in fna])
-            out_dict['GC'] = float(sum([str(s.seq).count("G")+str(s.seq).count("C") for s in fna]))/out_dict['length']
-            #out_dict['taxo:phylophlan'] = tax[bin_id]
-            return (bin_id, out_dict)
 
         dat = dict([process_bin(p) for p in tqdm(bin_files)])
         DataFrame.from_dict(dat, orient = 'index').to_csv(out_file)
